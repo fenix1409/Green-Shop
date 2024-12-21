8d54c0c4848ff71a4fe7c97846878daaef981969
fenix1409 <b2550486@gmail.com>
2024-12-21 14:18:43 +0500

    add

--- store/counterSlice.tsx
+++ store/counterSlice.tsx
@@ -1,11 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface OrderState {
-  items: { [key: string]: number };
+  items: { [key: string]: number }
 }
 
 const initialState: OrderState = {
-  items: {},
+  items: {}
 };
 
 const orderSlice = createSlice({
@@ -15,23 +15,23 @@ const orderSlice = createSlice({
     increment: (state, action: PayloadAction<string>) => {
       const productId = action.payload;
       if (state.items[productId]) {
-        state.items[productId] += 1;
+        state.items[productId] += 1
       } else {
-        state.items[productId] = 1;
+        state.items[productId] = 1
       }
     },
     decrement: (state, action: PayloadAction<string>) => {
-      const productId = action.payload;
+      const productId = action.payload
       if (state.items[productId] && state.items[productId] > 1) {
-        state.items[productId] -= 1;
+        state.items[productId] -= 1
       }
     },
     setCount: (state, action: PayloadAction<{ productId: string, count: number }>) => {
-      const { productId, count } = action.payload;
-      state.items[productId] = count;
+      const { productId, count } = action.payload
+      state.items[productId] = count
     },
   },
 });
 
-export const { increment, decrement, setCount } = orderSlice.actions;
-export default orderSlice.reducer;
+export const { increment, decrement, setCount } = orderSlice.actions
+export default orderSlice.reducer
